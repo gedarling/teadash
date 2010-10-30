47f8b474ff74d4124143c5c9e7c28a155667ddec
geoff <geoffreydarling@gmail.com>
2010-10-30 02:13:04 -0500

    today working with spore

--- lib/TeaDash/Web.pm
+++ lib/TeaDash/Web.pm
@@ -2,10 +2,16 @@
 use Web::Simple 'TeaDash::Web';
 {
   package TeaDash::Web;
-  use JSON::XS qw/encode_json decode_json/;
-  use LWP::Simple;
+  use Net::HTTP::Spore;
   use HTML::Zoom;
   use autodie;
+  
+  my $teatime = Net::HTTP::Spore->new_from_spec(
+    '/home/geoff/code/teadash/lib/TeaDash/teatime.json',
+    api_base_url => 'http://localhost:5000'
+  );
+  
+  $teatime->enable('Format::JSON');
   
   sub _derp {
     my $headers = shift;
@@ -20,34 +26,32 @@ use Web::Simple 'TeaDash::Web';
   }
   
   sub today {
-    my $current_tea = decode_json(
-      LWP::Simple::get('http://localhost:5000/current_tea')
-    );
+    my $current_tea = $teatime->current->body;
     
     return "Today's tea is $current_tea->{data}{name}";  
   }
+  #
+  #sub details {
+  #  my $stats = decode_json(
+  #    LWP::Simple::get('http://localhost:5000/stats')
+  #  );
+  #  
+  #  return $stats->{data};
+  #}
   
-  sub details {
-    my $stats = decode_json(
-      LWP::Simple::get('http://localhost:5000/stats')
-    );
-    
-    return $stats->{data};
-  }
-  
-  sub pie {
-    my $stats = decode_json(
-      LWP::Simple::get('http://localhost:5000/stats')
-    );
-    
-    my @return = map {
-      [ $_->{name}, $_->{count} ]
-    } @{ $stats->{data}};
-    
-    return _derp([ 'Content-type', 'text/json' ],
-      [ encode_json(\@return) ]
-    );
-  }
+  #sub pie {
+  #  my $stats = decode_json(
+  #    LWP::Simple::get('http://localhost:5000/stats')
+  #  );
+  #  
+  #  my @return = map {
+  #    [ $_->{name}, $_->{count} ]
+  #  } @{ $stats->{data}};
+  #  
+  #  return _derp([ 'Content-type', 'text/json' ],
+  #    [ encode_json(\@return) ]
+  #  );
+  #}
   
   sub main {
     my $zoom = HTML::Zoom->from_file('static/html/dash.html');
@@ -55,17 +59,17 @@ use Web::Simple 'TeaDash::Web';
     $zoom = $zoom->select('title,#today')
       ->replace_content($self->today);
     
-    $zoom = $zoom->select('#stats')
-      ->repeat_content([
-        map {
-          my $details = $_;
-          sub {
-            $_->select('.name')->replace_content($details->{name})
-              ->select('.count')->replace_content($details->{count});
-          }
-        } @{ $self->details }
-      ]);
-    
+    #$zoom = $zoom->select('#stats')
+    #  ->repeat_content([
+    #    map {
+    #      my $details = $_;
+    #      sub {
+    #        $_->select('.name')->replace_content($details->{name})
+    #          ->select('.count')->replace_content($details->{count});
+    #      }
+    #    } @{ $self->details }
+    #  ]);
+    #
     return _derp([ 'Content-type', 'text/html'], [$zoom->to_html] );
   }
   
