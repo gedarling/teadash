970e7f9d11e672a404538218afe1074eda084d33
geoff <geoffreydarling@gmail.com>
2011-04-11 21:37:32 -0500

    replace the pie graph with a tempory highcharts one

--- static/js/pie.js
+++ static/js/pie.js
@@ -1,39 +1,36 @@
-$.getJSON('/pie', function(data) {
-  var fields = [];
-  var labels = [];
-  $.each(data, function(idx,value){
-      labels.push('%%.%% - ' + value[0]);
-      fields.push(value[1]);
-  });
-  
-  var r = Raphael('pie_graph');
-  r.g.txtattr.font = "12px 'Fontin Sans', Fontin-Sans, sans-serif";
-  r.g.text(200, 10, "Tea Consumption").attr({"font-size": 20});
-  
-  var pie = r.g.piechart(
-    200,
-    160,
-    120,
-    fields,
-    {
-      legend: labels,
-      legendpos: "east"
-    }
-  );
-  
-  pie.hover(function () {
-    this.sector.stop();
-    this.sector.scale(1.1, 1.1, this.cx, this.cy);
-    if (this.label) {
-      this.label[0].stop();
-      this.label[0].scale(1.5);
-      this.label[1].attr({"font-weight": 800});
-    }
-  }, function () {
-    this.sector.animate({scale: [1, 1, this.cx, this.cy]}, 500, "bounce");
-    if (this.label) {
-      this.label[0].animate({scale: 1}, 500, "bounce");
-      this.label[1].attr({"font-weight": 400});
-    }
-  });
+$.getJSON('/pie', function(data) {  
+  var chart = new Highcharts.Chart({
+    chart: {
+      renderTo:'pie_graph',
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false
+    },
+    title: {
+      text: 'Tea Consumption'
+    },
+    tooltip: {
+      formatter: function() {
+        return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
+      }
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          color: '#000000',
+          connectorColor: '#000000',
+          formatter: function() {
+            return '<b>'+ this.point.name +'</b>: '+ this.y +' %';
+          }
+        }
+      }
+    },
+     series: [{
+      type: 'pie',
+      data: data
+    }]
+  });  
 });
